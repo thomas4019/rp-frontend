254da36d2d60b32bfdba70e4d2aba31aa32e9464
David Hughes <hughes.davidallen@gmail.com>
2017-05-29 07:19:10 -0700

    Add a component for the Terms page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Copyright from '@/components/Copyright'
+import Terms from '@/components/Terms'
 import UserProfile from '@/components/UserProfile'
 import UserEdit from '@/components/UserEdit'
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/profile',
       name: 'UserProfile',
       component: UserProfile
+    },
+    {
+      path: '/terms',
+      name: 'Terms',
+      component: Terms
     },
     {
       path: '/useredit',
