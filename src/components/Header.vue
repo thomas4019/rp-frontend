d3218a4905df6e8d222db63da699c2ffdf3ee5a2
Thomas Hansen <th4019@gmail.com>
2017-05-31 00:24:25 -0700

    fix login button in header, popup sizing broken

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,33 @@
 <template>
-<nav id="landing-nav">
-  <router-link to="/"><img src="../assets/logo2x.jpg" id="logo" /></router-link>
-  <ul id="primary-menu">
-    <li id="menu-about"><router-link to="/about">About</router-link></li>
-    <li class="hide-on-mobile" id="menu-find-races"><a href="/#choose-from">Find Races</a></li>
-    <li class="hide-on-mobile" id="menu-explore-passes"><a href="/#choose-pass">Explore Passes</a></li>
-    <li class="hide-on-mobile" id="menu-faq"><a href="/#faq">FAQ</a></li>
-    <li id="menu-blog"><a href="https://medium.com/racepass" target="_blank">Blog</a></li>
-    <li id="menu-directors"><router-link to="/race-directors">Race Directors</router-link></li>
-    <li role="separator" class="divider"></li>
-    <li class="hide-on-mobile" ><a id="get-racepass-button" class="button" href="/#choose-pass">Get Racepass</a></li>
-    <li role="separator" class="divider"></li>
-    <li><button class="hollow" @click="login()">Login</button></li>
-  </ul>
-</nav>
+<div>
+  <modal classes="rp-modal" :adaptive="true" :height="350" width="60%" name="login-header">
+    <button type="button" class="rp-modal-close" aria-label="Close" @click="$modal.hide('login-header')"><span aria-hidden="true">&times;</span></button>
+    <login :isCreation="false" />
+  </modal>
+  <nav id="landing-nav">
+    <router-link to="/"><img src="../assets/logo2x.jpg" id="logo" /></router-link>
+    <ul id="primary-menu">
+      <li id="menu-about"><router-link to="/about">About</router-link></li>
+      <li class="hide-on-mobile" id="menu-find-races"><a href="/#choose-from">Find Races</a></li>
+      <li class="hide-on-mobile" id="menu-explore-passes"><a href="/#choose-pass">Explore Passes</a></li>
+      <li class="hide-on-mobile" id="menu-faq"><a href="/#faq">FAQ</a></li>
+      <li id="menu-blog"><a href="https://medium.com/racepass" target="_blank">Blog</a></li>
+      <li id="menu-directors"><router-link to="/race-directors">Race Directors</router-link></li>
+      <li role="separator" class="divider"></li>
+      <li class="hide-on-mobile" ><a id="get-racepass-button" class="button" href="/#choose-pass">Get Racepass</a></li>
+      <li role="separator" class="divider"></li>
+      <li><button class="hollow" @click="$modal.show('login-header')">Login</button></li>
+    </ul>
+  </nav>
+</div>
 </template>
 
 <script>
+import Login from '@/components/Login'
 export default {
-
+  components: {
+    'login': Login,
+  },
 }
 </script>
 
