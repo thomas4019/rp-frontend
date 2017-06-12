ff1913b28a0b9d18b8c45b84d57573ecd433bf9e
David Hughes <hughes.davidallen@gmail.com>
2017-06-12 07:15:13 -0500

    Remove v-once from the footer component
    This was interfering with the logic for displaying the video attribution.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer v-once>
+  <footer>
     <div id="footer-top" class="container">
       <ul class="bottom-menu" style="padding-right: 20px;">
         <li class="header">Company</li>
