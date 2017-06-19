af6750e7b18660e106ebc60c2e58d852bf3e49a5
David Hughes <hughes.davidallen@gmail.com>
2017-06-19 06:29:10 -0500

    Move validation css to the UserEdit compenent

--- src/components/UserEdit.vue
+++ src/components/UserEdit.vue
@@ -323,4 +323,38 @@ export default {
 #emergency-contact-box input {
 	flex: 1;
 }
+
+input.validation-success,
+select.validation-success,
+textarea.validation-success {
+  color: #468847;
+  background-color: #DFF0D8;
+  border: 1px solid #D6E9C6;
+}
+
+input.validation-error,
+select.validation-error,
+textarea.validation-error {
+  color: #B94A48;
+  background-color: #F2DEDE;
+  border: 2px solid #EE9397;
+}
+
+.validation-errors-list {
+  margin: 2px 0 3px;
+  padding: 0;
+  list-style-type: none;
+  font-size: 0.9em;
+  line-height: 0.9em;
+  opacity: 0;
+
+  transition: all .3s ease-in;
+  -o-transition: all .3s ease-in;
+  -moz-transition: all .3s ease-in;
+  -webkit-transition: all .3s ease-in;
+}
+
+.validation-errors-list.filled {
+  opacity: 1;
+}
 </style>
