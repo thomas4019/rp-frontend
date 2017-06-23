c824a7ed1c6e47b402c9e5a3cde8958f57fd5b2e
Thomas Hansen <th4019@gmail.com>
2017-06-22 21:22:34 -0700

    add styleguide

---
+++ src/components/StyleGuide.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>Style Guide</h1>
+    <h2>This page serves as reference for the default styles for various components.</h2>
+    <div style="background-color: white; padding: 20px;">
+    <div style="color: #24272A;">#24272A - default background ██████████</div>
+    <div style="color: #323237;">#323237 - brigther background ██████████</div>
+    </div>
+    <div style="background-color: black; padding: 20px;">
+    <div style="color: #9B9B9B;">#9B9B9B - subtle text color ██████████</div>
+    <div style="color: #ABAEB7;">#ABAEB7 - paragraph text color ██████████</div>
+    <div style="color: #F7F7F7;">#F7F7F7 - default text color ██████████</div>
+    <div style="color: #0DFFAE;">#0DFFAE - action text ██████████</div>
+    </div>
+
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    <h4>Header 4</h4>
+    <em>em</em>
+    <p>This is a paragraph of text</p>
+    <div>This is normal text</div>
+    <strong>This is strong</strong><br><br>
+    <button>This is a button</button><br><br>
+    <button class="rounded">This is a rounded button</button><br><br>
+    <button class="hollow">This is a hollow button</button><br><br>
+    <input type="text" placeholder="Basic Input" /><br><br>
+    <textarea>This is a textarea</textarea><br><br>
+    <a href="#">This is a link</a>
+    <section class="solo">This is a solo section
+      <h3>Header 3</h3>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
